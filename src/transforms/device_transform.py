90560395dd308747d7f2aedfad23aaf4e17648d8
Dan Peachey <dan@netdevs.eu>
2025-09-27 13:03:39 +0200

    Fix docker compose

--- src/transforms/device_transform.py
+++ src/transforms/device_transform.py
@@ -10,6 +10,6 @@ class DeviceTransformYaml(InfrahubTransform):
     url: str = "device-yaml"
 
     async def transform(self, data: dict[str, Any]) -> dict[str, Any]:
-        templates_path = f"{self.root_directory}/transforms/templates"
+        templates_path = f"{self.root_directory}/src/transforms/templates"
         device: Device = Device.create(data=data, templates_path=templates_path)
         return device.yaml_config()
